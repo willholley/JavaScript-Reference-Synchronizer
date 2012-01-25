e006507d1c3c002105391137b858ec4e68752c82
Will Holley <willholley@gmail.com>
2012-01-25 12:12:34 +0700

    moved LineReplacer to the CassetteHelper project

---
+++ CassetteHelper/LineReplacer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace CassetteHelper
+{
+    public class LineReplacer
+    {
+        private readonly string newReference;
+        private readonly ILineVisitor visitor;
+
+        public LineReplacer(ILineVisitor visitor, string replacementUrl)
+        {
+            this.visitor = visitor;
+            this.newReference = string.Format("/// <reference path=\"{0}\" />", replacementUrl); 
+        }
+        
+        public void Replace(string targetFilePath)
+        {
+            if (string.IsNullOrEmpty(targetFilePath)) throw new ArgumentNullException("targetFilePath");
+            if (!File.Exists(targetFilePath)) throw new FileNotFoundException("Could not find file to replace lines in", targetFilePath);
+            
+            using (var stringWriter = new StringWriter())
+            {
+                bool containsReferences;
+
+                using (var inputStream = File.OpenText(targetFilePath))
+                {
+                    containsReferences = visitor.Visit(inputStream, 
+                                                       r => stringWriter.WriteLine(this.newReference),
+                                                       stringWriter.WriteLine);
+                }
+
+                if (containsReferences)
+                {
+                    File.WriteAllText(targetFilePath, stringWriter.ToString());
+                }
+            }
+        }
+    }
+}
