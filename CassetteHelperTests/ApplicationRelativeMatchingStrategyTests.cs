0a7bbdd31e7620d606a3c1a0dd8e4d6d541114c5
Will Holley <willholley@gmail.com>
2012-01-25 14:01:25 +0700

    incomplete work on application relative strategy tests

--- CassetteHelperTests/ApplicationRelativeMatchingStrategyTests.cs
+++ CassetteHelperTests/ApplicationRelativeMatchingStrategyTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
+using CassetteHelper;
 using MbUnit.Framework;
 
 namespace CassetteHelperTests
@@ -14,7 +16,7 @@ namespace CassetteHelperTests
         [Row(@"c:\doesNotExist", ExpectedException = typeof(ArgumentException), ExpectedExceptionMessage = "applicationRootDirectory")]
         public void ApplicationRootMustBeValid(string applicationRootDirectory)
         {
-            new ApplicationRelativeMatchingStrategy(applicationRootDirectory, "original.js", "new.js");
+            new ApplicationRelativeMatchingStrategy(applicationRootDirectory, "original.js");
         }
 
         [Test, ExpectedArgumentNullException("originalFilePath")]
@@ -22,30 +24,48 @@ namespace CassetteHelperTests
         [Row("")]
         public void OriginalFilePathMustNotBeNullOrEmpty(string originalFilePath)
         {
-            new ApplicationRelativeMatchingStrategy(@"c:\", originalFilePath, "new.js");
+            new ApplicationRelativeMatchingStrategy(@"c:\", originalFilePath);
         }
 
-        [Test, ExpectedArgumentNullException("newFilePath")]
-        [Row(null)]
-        [Row("")]
-        public void NewFilePathMustNotBeNullOrEmpty(string newFilePath)
+        [Test]
+        [Row("/// not a match", false)]
+        [Row(null, false)]
+        [Row("", false)]
+        [Row("function foo()", false)]
+        [Row("/// <reference path=\"~/test/original.js\" />", true)]
+        [Row("/// <reference path=\"~/Test/Original.js\" />", true)]        // case insensitive
+        //[Row("  /// <reference path=\"~/test/original.js\" />", true)]    // whitespace at beginning
+        [Row("///<reference  path=\"~/test/original.js\" />", true)]        // whitespace before path
+        [Row("///<reference path=\"~/test/original.js\"  />", true)]        // whitespace before close tag
+        [Row("///<reference path=\"~/test/original.js\"/>", true)]          // no whitespace before close tag
+        [Row("function foo() /// <reference path=\"~/test/original.js\" />", false)]
+        [Row("/// <reference path=\"~/test/notoriginal.js\" />", false)]    // wrong file
+        public void Match(string line, bool expected)
         {
-            new ApplicationRelativeMatchingStrategy(@"c:\", "original.js", newFilePath);
+            var strategy = new ApplicationRelativeMatchingStrategy(@"c:\", @"c:\test\original.js");
+            Assert.AreEqual(expected, strategy.Match(line));
         }
     }
 
-    public class ApplicationRelativeMatchingStrategy
+    public class ApplicationRelativeMatchingStrategy : IMatchingStrategy
     {
-        public ApplicationRelativeMatchingStrategy(string applicationRootDirectory, string originalFilePath, string newFilePath)
+        private readonly Regex matchingRegex;
+
+        public ApplicationRelativeMatchingStrategy(string applicationRootDirectory, string originalFilePath)
         {
             if (String.IsNullOrEmpty(applicationRootDirectory)) throw new ArgumentNullException("applicationRootDirectory");
             if (String.IsNullOrEmpty(originalFilePath)) throw new ArgumentNullException("originalFilePath");
-            if (String.IsNullOrEmpty(newFilePath)) throw new ArgumentNullException("newFilePath");
 
             if (!Directory.Exists(applicationRootDirectory)) throw new ArgumentException("Could not find directory '" + applicationRootDirectory + "'", "applicationRootDirectory");
 
+            var applicationRelativePath = originalFilePath.URIRelativeTo(applicationRootDirectory);
+            this.matchingRegex = new Regex("^///\\W+<reference\\W+path=\"~/" + applicationRelativePath + "\"\\W+/>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            //this.matchingRegex = new Regex("^///\\W+<reference path=\"~/(?<path>[^\"]+)\"\\W+/>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         }
-
-
+        
+        public bool Match(string line)
+        {
+            return matchingRegex.IsMatch(line);
+        }
     }
 }
