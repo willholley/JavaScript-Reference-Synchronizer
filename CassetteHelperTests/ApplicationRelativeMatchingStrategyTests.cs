fcc2815caa608bab7ca4198656cde9ec55a0ea7b
Will Holley <willholley@gmail.com>
2012-02-02 15:42:10 +0530

    fixed ApplicationRelativeMatchingStrategy tests

--- CassetteHelperTests/ApplicationRelativeMatchingStrategyTests.cs
+++ CassetteHelperTests/ApplicationRelativeMatchingStrategyTests.cs
@@ -34,7 +34,7 @@ namespace CassetteHelperTests
         [Row("function foo()", false)]
         [Row("/// <reference path=\"~/test/original.js\" />", true)]
         [Row("/// <reference path=\"~/Test/Original.js\" />", true)]        // case insensitive
-        //[Row("  /// <reference path=\"~/test/original.js\" />", true)]    // whitespace at beginning
+        [Row("  /// <reference path=\"~/test/original.js\" />", true)]    // whitespace at beginning
         [Row("///<reference  path=\"~/test/original.js\" />", true)]        // whitespace before path
         [Row("///<reference path=\"~/test/original.js\"  />", true)]        // whitespace before close tag
         [Row("///<reference path=\"~/test/original.js\"/>", true)]          // no whitespace before close tag
@@ -59,13 +59,13 @@ namespace CassetteHelperTests
             if (!Directory.Exists(applicationRootDirectory)) throw new ArgumentException("Could not find directory '" + applicationRootDirectory + "'", "applicationRootDirectory");
 
             var applicationRelativePath = originalFilePath.URIRelativeTo(applicationRootDirectory);
-            this.matchingRegex = new Regex("^///\\W+<reference\\W+path=\"~/" + applicationRelativePath + "\"\\W+/>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            this.matchingRegex = new Regex("^\\W*///\\W*<reference\\W+path=\"~/" + applicationRelativePath + "\"\\W*/>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
             //this.matchingRegex = new Regex("^///\\W+<reference path=\"~/(?<path>[^\"]+)\"\\W+/>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         }
         
         public bool Match(string line)
         {
-            return matchingRegex.IsMatch(line);
+            return !String.IsNullOrWhiteSpace(line) && matchingRegex.IsMatch(line);
         }
     }
 }
