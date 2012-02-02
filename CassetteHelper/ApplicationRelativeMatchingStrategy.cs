b548e6aadcb8224e0a3efefc891f7290a83e1642
Will Holley <willholley@gmail.com>
2012-02-02 15:43:31 +0530

    moved ApplicationRealiveMatchingStrategy into main project

---
+++ CassetteHelper/ApplicationRelativeMatchingStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CassetteHelper
+{
+    public class ApplicationRelativeMatchingStrategy : IMatchingStrategy
+    {
+        private readonly Regex matchingRegex;
+
+        public ApplicationRelativeMatchingStrategy(string applicationRootDirectory, string originalFilePath)
+        {
+            if (String.IsNullOrEmpty(applicationRootDirectory)) throw new ArgumentNullException("applicationRootDirectory");
+            if (String.IsNullOrEmpty(originalFilePath)) throw new ArgumentNullException("originalFilePath");
+
+            if (!Directory.Exists(applicationRootDirectory)) throw new ArgumentException("Could not find directory '" + applicationRootDirectory + "'", "applicationRootDirectory");
+
+            var applicationRelativePath = originalFilePath.URIRelativeTo(applicationRootDirectory);
+            this.matchingRegex = new Regex("^\\W*///\\W*<reference\\W+path=\"~/" + applicationRelativePath + "\"\\W*/>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            //this.matchingRegex = new Regex("^///\\W+<reference path=\"~/(?<path>[^\"]+)\"\\W+/>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        }
+        
+        public bool Match(string line)
+        {
+            return !String.IsNullOrWhiteSpace(line) && matchingRegex.IsMatch(line);
+        }
+    }
+}
--- CassetteHelperTests/ApplicationRelativeMatchingStrategyTests.cs
+++ CassetteHelperTests/ApplicationRelativeMatchingStrategyTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Text.RegularExpressions;
 using CassetteHelper;
 using MbUnit.Framework;
 
@@ -44,28 +42,6 @@ namespace CassetteHelperTests
         {
             var strategy = new ApplicationRelativeMatchingStrategy(@"c:\", @"c:\test\original.js");
             Assert.AreEqual(expected, strategy.Match(line));
-        }
-    }
-
-    public class ApplicationRelativeMatchingStrategy : IMatchingStrategy
-    {
-        private readonly Regex matchingRegex;
-
-        public ApplicationRelativeMatchingStrategy(string applicationRootDirectory, string originalFilePath)
-        {
-            if (String.IsNullOrEmpty(applicationRootDirectory)) throw new ArgumentNullException("applicationRootDirectory");
-            if (String.IsNullOrEmpty(originalFilePath)) throw new ArgumentNullException("originalFilePath");
-
-            if (!Directory.Exists(applicationRootDirectory)) throw new ArgumentException("Could not find directory '" + applicationRootDirectory + "'", "applicationRootDirectory");
-
-            var applicationRelativePath = originalFilePath.URIRelativeTo(applicationRootDirectory);
-            this.matchingRegex = new Regex("^\\W*///\\W*<reference\\W+path=\"~/" + applicationRelativePath + "\"\\W*/>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-            //this.matchingRegex = new Regex("^///\\W+<reference path=\"~/(?<path>[^\"]+)\"\\W+/>", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-        }
-        
-        public bool Match(string line)
-        {
-            return !String.IsNullOrWhiteSpace(line) && matchingRegex.IsMatch(line);
         }
     }
 }
