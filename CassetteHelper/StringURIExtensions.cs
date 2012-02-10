be86a8447abe42ea2b515b5e1c09475c94331cae
Will Holley <willholley@gmail.com>
2012-02-10 22:20:40 +0530

    Now matches correctly but replacement is not application relative so does not match again. Should ideally replace like for like.

--- CassetteHelper/StringURIExtensions.cs
+++ CassetteHelper/StringURIExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CassetteHelper
 {
@@ -6,7 +7,18 @@ namespace CassetteHelper
     {
         public static string URIRelativeTo(this string filePath, string baseFile)
         {
+            baseFile = AssertTrailingSlash(baseFile);
+
             return new Uri(baseFile).MakeRelativeUri(new Uri(filePath)).ToString();
+        }
+
+        private static string AssertTrailingSlash(string baseFile)
+        {
+            if(Path.GetExtension(baseFile) == "" && !baseFile.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                baseFile += Path.DirectorySeparatorChar;
+            }
+            return baseFile;
         }
     }
 }
--- CassetteHelperTests/StringURIExtensionsTests.cs
+++ CassetteHelperTests/StringURIExtensionsTests.cs
@@ -13,6 +13,7 @@ namespace CassetteHelperTests
         [Row(@"c:\", @"c:\test\foo.js", "test/foo.js")] // subdirectory
         [Row(@"c:\test.js", @"c:\foo.js", "foo.js")] // sibling
         [Row(@"c:\test\foo.js", @"c:\foo.js", "../foo.js")] // parent
+        [Row(@"c:\test", @"c:\test\inner", "inner")] // directory no trailing slash
         public void URIRelativeTo(string baseFile, string currentFile, string expected)
         {
             Assert.AreEqual(expected, currentFile.URIRelativeTo(baseFile));
